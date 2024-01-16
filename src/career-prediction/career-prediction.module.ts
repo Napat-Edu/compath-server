a048298498c35b7c4cdfbebc4a7db9e16f90108e
Napat Wareedee <omegyzr@gmail.com>
2024-01-16 23:38:10 +0700

    [TEMP] disable mongoose

--- src/career-prediction/career-prediction.module.ts
+++ src/career-prediction/career-prediction.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CareerPredictionController } from './career-prediction.controller';
 import { CareerPredictionService } from './career-prediction.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import {
-  ResumeHistory,
-  ResumeHistorySchema,
-} from 'src/schemas/resume-history.schema';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import {
+//   ResumeHistory,
+//   ResumeHistorySchema,
+// } from 'src/schemas/resume-history.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: ResumeHistory.name, schema: ResumeHistorySchema },
-    ]),
+    // MongooseModule.forFeature([
+    //   { name: ResumeHistory.name, schema: ResumeHistorySchema },
+    // ]),
   ],
   controllers: [CareerPredictionController],
   providers: [CareerPredictionService],
