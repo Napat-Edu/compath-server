0dd4ad104205a948320eba81a089d257ab2f56e6
Napat Wareedee <omegyzr@gmail.com>
2024-01-16 23:29:28 +0700

    [TEST] logging test env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { msg: 'Hello World!!' };
+    return { msg: "this is mode : " + process.env.MODE };
   }
 }
