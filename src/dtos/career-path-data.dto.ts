1bd96bae78583e3f42cbc72d7e82cf6faca10c8c
Napat-Edu <omegyzr@gmail.com>
2024-02-23 17:35:58 +0700

    [UPDATE] restructure interface

--- src/dtos/career-path-data.dto.ts
+++ src/dtos/career-path-data.dto.ts
@@ -1,14 +1,17 @@
-export interface CareerPathDataDto {
-  career_path_name: string;
-  career_path_description: string;
+export interface CareerPathDataDto extends ICareerPathBaseData {
   related_careers: CareerDto[];
-  base_salary: ISalary;
-  icon_svg: string;
 }
 
 export interface CareerDto {
   career: string;
   skill_domains: string[];
+}
+
+export interface ICareerPathBaseData {
+  career_path_name: string;
+  career_path_description: string;
+  base_salary: ISalary;
+  icon_svg: string;
 }
 
 export interface ISalary {
