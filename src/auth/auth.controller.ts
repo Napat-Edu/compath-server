3c65f884907167d179b8cea8ebc04bebce304f8b
Napat Wareedee <omegyzr@gmail.com>
2024-04-16 00:07:16 +0700

    add manage exchange token phase before verify user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,13 +12,38 @@ const client = new OAuth2Client(
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('google/login')
   // @UseGuards(GoogleAuthGuard)
-  async handleLogin(@Body('token') token: string) {
+  async handleLogin(@Body('code') code: string) {
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+    const redirect_uri = 'http://localhost:3000';
+    const grant_type = 'authorization_code';
+
+    const credential = await fetch('https://oauth2.googleapis.com/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        code,
+        client_id,
+        client_secret,
+        redirect_uri,
+        grant_type,
+      }),
+    })
+      .then(response => response.json())
+      .catch(error => {
+        console.log(error);
+
+        return { error: error };
+      });
+
     const ticket = await client.verifyIdToken({
-      idToken: token,
+      idToken: credential.id_token,
       audience: process.env.CLIENT_ID,
     });
     const payload = ticket.getPayload();
