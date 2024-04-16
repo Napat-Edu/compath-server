dfc9d26000c6541e205e3ab5d2c56b6eec27b789
Napat-Edu <omegyzr@gmail.com>
2024-04-16 16:06:06 +0700

    add DELETE permission

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [process.env.BASE_CLIENT],
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
   });
   app.use(
