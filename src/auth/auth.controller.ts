f5f867b007b10dbcc31f71af36c6ecaca4061981
Napat-Edu <omegyzr@gmail.com>
2024-04-16 13:59:14 +0700

    create exchange token method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-// import { GoogleAuthGuard } from './guards/google.guard';
-// import { Request } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OAuth2Client } from 'google-auth-library';
 import { AuthService } from './auth.service';
 import { UserDto } from 'src/dtos/user.dto';
@@ -15,35 +13,10 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('google/login')
-  // @UseGuards(GoogleAuthGuard)
   async handleLogin(@Body('code') code: string) {
-    const client_id = process.env.CLIENT_ID;
-    const client_secret = process.env.CLIENT_SECRET;
-    const redirect_uri = process.env.BASE_CLIENT;
-    const grant_type = 'authorization_code';
-
-    const credential = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        code,
-        client_id,
-        client_secret,
-        redirect_uri,
-        grant_type,
-      }),
-    })
-      .then((response) => response.json())
-      .catch((error) => {
-        console.log(error);
-
-        return { error: error };
-      });
-
+    const token = await this.authService.exchangeToken(code);
     const ticket = await client.verifyIdToken({
-      idToken: credential.id_token,
+      idToken: token.id_token,
       audience: process.env.CLIENT_ID,
     });
     const payload = ticket.getPayload();
@@ -52,8 +25,8 @@ export class AuthController {
       email: payload.email,
       displayName: payload.name,
     };
-    const userData = await this.authService.login(userPayload);
 
+    const userData = await this.authService.login(userPayload);
     const response = {
       id: userData.id,
       email: userData.email,
@@ -62,17 +35,4 @@ export class AuthController {
     };
     return response;
   }
-
-  // @Get('google/redirect')
-  // @UseGuards(GoogleAuthGuard)
-  // handleRedirect(@Req() request: Request) {
-  //   return request.user;
-  // }
-
-  // @Get('status')
-  // user(@Req() request: Request) {
-  //   return request.user
-  //     ? { msg: 'Authenticated' }
-  //     : { msg: 'Not Authenticated' };
-  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { AxiosError } from 'axios';
 import { Model } from 'mongoose';
+import { catchError, firstValueFrom } from 'rxjs';
 import { UserDto } from 'src/dtos/user.dto';
 import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger();
+
   constructor(
     @InjectModel(User.name)
     private userModel: Model<UserDocument>,
+    private readonly httpService: HttpService,
   ) {}
+
+  async exchangeToken(code: string) {
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+    const redirect_uri = process.env.BASE_CLIENT;
+    const grant_type = 'authorization_code';
+
+    const bodyParams = new URLSearchParams({
+      code,
+      client_id,
+      client_secret,
+      redirect_uri,
+      grant_type,
+    });
+
+    const token = await firstValueFrom(
+      this.httpService
+        .post('https://oauth2.googleapis.com/token', bodyParams)
+        .pipe(
+          catchError((err: AxiosError) => {
+            this.logger.error(err.response.data);
+            throw 'error occured';
+          }),
+        ),
+    );
+
+    return token.data;
+  }
 
   async login(details: UserDto) {
     const user = await this.userModel.findOne({ email: details.email });
@@ -19,20 +53,5 @@ export class AuthService {
       const newUser = new this.userModel(details);
       return newUser.save();
     }
-  }
-
-  async validateUser(details: UserDto, picture: string) {
-    const user = await this.userModel.findOne({ email: details.email });
-    if (user) {
-      return user;
-    } else {
-      const newUser = new this.userModel(details);
-      return newUser.save();
-    }
-  }
-
-  async findUser(id: string) {
-    const user = await this.userModel.findOne({ id: id });
-    return user;
   }
 }
