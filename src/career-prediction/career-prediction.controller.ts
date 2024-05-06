3b9cfaf5899344c248c98fa425ab13acb1db3ee2
Napat-Edu <omegyzr@gmail.com>
2024-05-06 13:01:50 +0700

    update ocr api method

--- src/career-prediction/career-prediction.controller.ts
+++ src/career-prediction/career-prediction.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CareerPredictionService } from './career-prediction.service';
 import { IUserResumeInput } from 'src/interfaces/career-prediction.interface';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('/career-prediction')
 export class CareerPredictionController {
@@ -16,6 +26,26 @@ export class CareerPredictionController {
       );
     } catch (err) {
       return err;
+    }
+  }
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  createCareerPredictionByPDF(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000000 }),
+          new FileTypeValidator({ fileType: 'pdf' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    try {
+      return this.careerPredictionService.createCareerPredictionByPDF(file);
+    } catch (error) {
+      return error;
     }
   }
 }
